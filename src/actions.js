87421ae4adddc0f4211df7c26d84135aaa76fcbc
laura.beatriz <laura.beatriz@ekaizen.digital>
2022-05-05 16:01:06 -0300

    05-05: Implementando login

---
+++ src/actions.js
@@ -0,0 +1,21 @@
+import myaxios from './myaxios'
+
+export const loginUser = vitoria => {
+  return {
+    type: 'login/loginUser',
+    vitoria
+  }
+}
+
+export async function reduxLogin({ googleToken, fcmToken }) {
+  return async dispatch => {
+    const response = await myaxios.post('/auth/loginWithGoogle', {
+      googleToken
+    })
+    const token = response.data.token
+    console.log(token)
+    localStorage.setItem('token', response.data.token)
+    await myaxios.post('/messaging/register', { token: fcmToken })
+    dispatch(loginUser('legal'))
+  }
+}
